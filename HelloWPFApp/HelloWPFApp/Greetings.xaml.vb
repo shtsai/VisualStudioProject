0eb3febc6e8a570acfefa7dccb259f3071483447
Sean Tsai <shtsai7@gmail.com>
2016-07-21 12:10:48 -0400

    add HelloWPFApp

---
+++ HelloWPFApp/HelloWPFApp/Greetings.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class MainWindow
+    Private Sub button_Click(sender As Object, e As RoutedEventArgs) Handles button.Click
+        If radioButton1.IsChecked = True Then
+            MessageBox.Show("Hello.")
+        Else radioButton2.IsChecked = True
+            MessageBox.Show("Goodbye.")
+        End If
+    End Sub
+End Class
