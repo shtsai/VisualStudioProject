18fdbacb0446b175b2bfdcf81c14b36e5f07e040
Sean Tsai <seantsai726@gmail.com>
2016-07-23 12:00:52 -0400

    complete four quiz

--- MathQuiz/MathQuiz/Form1.cs
+++ MathQuiz/MathQuiz/Form1.cs
@@ -16,5 +16,120 @@ namespace MathQuiz
         {
             InitializeComponent();
         }
+
+        Random randomizer = new Random();
+
+        int addend1;
+        int addend2;
+        int minuend;
+        int subtrahend;
+        int multiplicand;
+        int multiplier;
+        int dividend;
+        int divisor;
+
+        // this integer keeps track of the remaining time
+        int timeLeft;
+
+        public void StartTheQuiz()
+        {
+            // addition problem
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+            sum.Value = 0;
+
+            // subtraction problem
+            minuend = randomizer.Next(1, 101);
+            subtrahend = randomizer.Next(1, minuend);
+
+            minusLeftLabel.Text = minuend.ToString();
+            minusRightLabel.Text = subtrahend.ToString();
+            difference.Value = 0;
+
+            // multiplication problem
+            multiplicand = randomizer.Next(2, 11);
+            multiplier = randomizer.Next(2, 11);
+
+            timesLeftLabel.Text = multiplicand.ToString();
+            timesRightLabel.Text = multiplier.ToString();
+            product.Value = 0;
+
+            // division problem
+            divisor = randomizer.Next(2, 11);
+            int temp = randomizer.Next(2, 11);
+            dividend = divisor * temp;
+
+            dividedLeftLabel.Text = dividend.ToString();
+            dividedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
+
+            // Start the timer
+            timeLeft = 15;
+            timeLabel.Text = "15 seconds";
+            timer1.Start();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (CheckTheAnswer())
+            {
+                timer1.Stop();
+                MessageBox.Show("Smart Ass! You got all the answers correct!", "Congratulation!");
+                startButton.Enabled = true;
+                timeLabel.BackColor = Color.Transparent;
+            }
+            else if (timeLeft > 0)
+            {
+                timeLeft -= 1;
+                timeLabel.Text = timeLeft + " seconds";
+                if (timeLeft < 10)
+                {
+                    timeLabel.BackColor = Color.Red;
+                }
+            }
+            else
+            {
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry!");
+                sum.Value = addend1 + addend2;
+                difference.Value = minuend - subtrahend;
+                product.Value = multiplicand * multiplier;
+                quotient.Value = dividend / divisor;
+                startButton.Enabled = true;
+                timeLabel.BackColor = Color.Transparent;
+            }
+        }
+
+        private bool CheckTheAnswer()
+        {
+            if ((addend1 + addend2 == sum.Value)
+                && (minuend - subtrahend == difference.Value)
+                && (multiplicand * multiplier == product.Value)
+                && (dividend / divisor == quotient.Value))
+                return true;
+            else
+                return false;
+        }
+
+        private void answer_Enter(object sender, EventArgs e)
+        {
+            NumericUpDown answerBox = sender as NumericUpDown;
+
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
+            }
+        }
     }
 }
